db64f11e752760b6ee6d0449d5659bd1604e7ace
mhsong <mhsong@kookmin.ac.kr>
2024-12-26 03:03:52 +0900

    Fix monitoring issues

--- IaC/module.tf
+++ IaC/module.tf
@@ -37,5 +37,5 @@ module "prometheus" {
   count             = var.enable_prometheus ? 1 : 0
   source            = "./prometheus"
 
-  depends_on = [module.eks]
+  depends_on = [module.eks, helm_release.nginx-ingress-controller]
 }
